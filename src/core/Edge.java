eb839bb02a9865c5d073c4c4df40cd95c689173d
gnap1 <58813841+gnap1@users.noreply.github.com>
2019-12-12 13:44:02 +0100

    Create Edge.java

---
+++ src/core/Edge.java
@@ -0,0 +1,38 @@
+package core;
+
+public class Edge {
+
+	private int source;
+	private int target;
+	private int weight;
+	private int id;
+	
+	public int getSource() {
+		return source;
+	}
+	public void setSource(int source) {
+		this.source = source;
+	}
+	public int getTarget() {
+		return target;
+	}
+	public void setTarget(int target) {
+		this.target = target;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	
+	
+	
+}
